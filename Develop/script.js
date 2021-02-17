8e15ca8b44ca0b811e39999fb26271a108e9d450
Tim Weyel <tweyel@gmail.com>
2021-02-17 14:54:26 -0800

    pseudocode

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,17 @@
+// need a series of prompts that ask the user for the following:
+    // --password length (8-128 chars)
+      //--store as a var
+    // --character types (lowercase, uppercase, numeric, and/or special chars)
+      //--create an object that stores all the char types
+      //--store each selection in a variable
+// --each answer should be validated
+// --password should include at least one of each character type
+// --when all prompts are answered, then password is generated
+// --password should be displayed as an alert or written to the page
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
