43927119465576d1ae3e2a91cf3b446a8d3e9bc5
Tim Weyel <tweyel@gmail.com>
2021-02-17 15:14:58 -0800

    need to restart computer

--- Develop/script.js
+++ Develop/script.js
@@ -9,15 +9,27 @@
 // --when all prompts are answered, then password is generated
 // --password should be displayed as an alert or written to the page
 
+//=======================================================================================
 
+// object used to create password criteria
 
+var passwordCriteria = {
+  
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // prompt for password length
+  var passwordLength = window.prompt("How long would you like your password to be? Select a length from 8 to 128.")
+  //console.log(passwordLength);
+
+  var 
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
