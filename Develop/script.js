5afec6692f73fb361ef3201eb122600de2dcb8ab
Tim Weyel <tweyel@gmail.com>
2021-02-17 14:43:45 -0800

    starting over

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
-
 // Write password to the #password input
 function writePassword() {
-  // prompt for password length
-  var passwordLength = window.prompt("How long would you like your password to be? Select a length from 8 to 128.")
-  //console.log(passwordLength);
-
-  var
-  //var password = generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
