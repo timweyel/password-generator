c382c5fb3c781f6fdb7f4197df8eed753eb33db2
Tim Weyel <tweyel@gmail.com>
2021-02-18 10:12:06 -0800

    password requirement prompts added

--- Develop/script.js
+++ Develop/script.js
@@ -8,33 +8,62 @@
 // --password should include at least one of each character type
 // --when all prompts are answered, then password is generated
 // --password should be displayed as an alert or written to the page
+// --find edge/break cases
+
+// Output - a mix of user defined, random characters, at least one from each type, in the user specified length
+// Input - user determines password length and up to 4 different character types
+// Code - 
+// Edge cases - 
+
 
 //=======================================================================================
 
-// object used to create password criteria
+// global variables
 
-var passwordCriteria = {
-  
-}
+var passwordLength; //stores the length of password
+var includeLowercase; //
+var includeUppercase;
+var includeNumeric;
+var includeSpecial;
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
 // Write password to the #password input
 function writePassword() {
-  // prompt for password length
-  var passwordLength = window.prompt("How long would you like your password to be? Select a length from 8 to 128.")
-  //console.log(passwordLength);
 
-  var 
-  //var password = generatePassword();
+  // prompt for password length
+  passwordLength = window.prompt("How long would you like your password to be? Select a length from 8 to 128.");
+  console.log(passwordLength);
+  // confirm to include lowercase characters
+  includeLowercase = window.confirm("Select OK if you want to include lowercase characters.");
+  console.log(includeLowercase);
+
+  //TODO: need to account for numbers outside of 8 to 128
+  
+  // confirm to include uppercase characters
+  includeUppercase = window.confirm("Select OK if you want to include uppercase characters.");
+  console.log(includeUppercase);
+  
+  // confirm to include numeric characters    
+  includeNumeric = window.confirm("Select OK if you want to include numeric characters.");
+  console.log(includeNumeric);
+
+  // confirm to include special characters
+  includeSpecial = window.confirm("Select OK if you want to include special characters.");
+  console.log(includeSpecial);
+    
+    
+  
+  
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
